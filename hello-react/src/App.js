c92a6b72056b22a1f64add15b8a188e7f31fa747
HomieKim <ghals0708@gmail.com>
2021-10-30 16:35:55 +0900

    7. LifeCycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -8,11 +8,29 @@ import React, { Component } from 'react';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSimple from './IterationSimple';
+import LifeCycle from './LifeCycle';
+import ErrorBoundary from './ErrorBoundary';
 
+function getRandomColor(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 class App extends Component{
+  state={
+    color:'#000000'
+  }
+  handleClick = ()=>{
+    this.setState({
+      color:getRandomColor()
+    });
+  }
   render(){
     return(
-     <IterationSimple/>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+        <LifeCycle color={this.state.color}/>
+        </ErrorBoundary>
+     </div>
     );
   }
 }
