4cc0c731444e5612caa390f2dc9d1bdba744b26b
HomieKim <ghals0708@gmail.com>
2021-11-09 23:23:19 +0900

    todo ui

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './component/TodoTemplate';
+import TodoInsert from './component/TodoInsert';
 
-const App = ()=>{
-  return <div>Todo App을 만들자</div>
+const App = () => {
+  return (
+    <TodoTemplate>
+      <TodoInsert/>
+    </TodoTemplate>
+  )
 };
 
 export default App;
