83fa296d03375f31246c4cf48dfe61f80928d702
HomieKim <ghals0708@gmail.com>
2021-11-10 23:41:50 +0900

    set list

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,11 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
+import React,{ useState } from 'react';
 
 const App = () => {
+  const [todos, setTodos]= useState([
+    {
+      id:1,
+      text : '리액트 기초 알아보기',
+      checked : true,
+    },
+    {
+      id:2,
+      text : '컴포넌트 스타일링 해보기',
+      checked : true,
+    },
+    {
+      id:3,
+      text : '일정 관리 앱 만들어 보기',
+      checked : false,
+    }
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   )
 };
