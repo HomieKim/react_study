5eb57788e767f63a348507763c4e9ef5127f2e49
HomieKim <ghals0708@gmail.com>
2021-11-20 23:32:30 +0900

    13. router : URL params

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router';
 import Home from './Home';
 import About from './About';
 import { Link } from 'react-router-dom';
+import Profile from './Profile';
 
 const App = ()=>{
   return (
@@ -15,10 +16,17 @@ const App = ()=>{
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact/>
       <Route path={['/about', '/info']} component={About}/>
+      <Route path="/profile/:username" component={Profile}/>
     </div>
   )
 }
