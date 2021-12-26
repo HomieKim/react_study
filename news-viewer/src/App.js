bc1e9e51e9ae000348e0e97426af125c5c89f8e4
HomieKim <ghals0708@gmail.com>
2021-12-26 21:00:40 +0900

    useState, useEffect 사용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,21 +1,29 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import Categories from './component/Categories';
 import NewsList from './component/NewsList';
 
 
 const App = () =>{
-  const [data,setData] = useState(null);
-  const onClick = async ()=>{
-    try{
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=61d605f4d5a44b8e86837c7e87acf0cc',);
-      setData(response.data)
-    }catch(e){
-      console.log(e);
-    }
-  };
+  // const [data,setData] = useState(null);
+  // const onClick = async ()=>{
+  //   try{
+  //     const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=61d605f4d5a44b8e86837c7e87acf0cc',);
+  //     setData(response.data)
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+  // };
+
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category),[]);
 
   return  (
-    <NewsList/>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+    
   );
 };
 
--- news-viewer/src/component/NewsList.js
+++ news-viewer/src/component/NewsList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 
@@ -22,16 +23,44 @@ const sampleArticle = {
     urlToImage: 'https://via.placeholder.com/160',
 };
 
-const NewsList = () => {
+const NewsList = ({category}) => {
+
+    const [articles, setArticles] = useState(null);
+    const [loading, SetLoading] = useState(false);
+
+    useEffect(()=>{
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () =>{
+            SetLoading(true);
+            try{
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const response = await axios.get(
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=61d605f4d5a44b8e86837c7e87acf0cc`
+                ,);
+                setArticles(response.data.articles);
+            }catch(e){
+                console.log(e);
+            }
+            SetLoading(false);
+        };
+        fetchData();
+    },[category]);
+
+    // 대기 중일 때
+    if(loading){
+        return <NewsListBlock>대기 중 ...</NewsListBlock>;
+    }
+    // 아직 acticles 값이 설정되지 않았을 때
+    if(!articles){
+        return null;
+    }
+
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article}/>
+            ))}
         </NewsListBlock>
 
     )
