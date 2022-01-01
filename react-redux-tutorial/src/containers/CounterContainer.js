352dd737f337822d1dc93ef2bf8392a685433e6a
HomieKim <ghals0708@gmail.com>
2022-01-01 20:42:44 +0900

    17. use redux Hooks

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 // 컴포넌트를 리덕스와 연동하려면 connect함수를 사용합니다. 
 // connect함수는 함수를 반환하는데 반환받음 함수에 타겟컴포넌트를 파라미터로 넣어주면 리덕스와 연동된 컴포넌트가 만들어집니다.
 
-const CounterContainer =({increase, decrease, number})=>{
+const CounterContainer =()=>{
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter 
+            number={number} 
+            onIncrease={onIncrease} 
+            onDecrease={onDecrease}/>
     ); 
 };
+
+export default CounterContainer;
 
 // const mapStateToProps = state => ({
 //     number: state.counter.number,
@@ -25,6 +34,8 @@ const CounterContainer =({increase, decrease, number})=>{
 //     },
 // });
 
+// connect 함수 사용
+/*
 export default connect(
     state =>({
         number : state.counter.number,
@@ -34,3 +45,4 @@ export default connect(
         decrease,
     },
 )(CounterContainer);
+*/
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-action';
+import { createAction , handleActions } from 'redux-action';
 
 // 액션타입 정의
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값을 변경함
@@ -11,7 +11,7 @@ export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id= 3;
 
-export const insert = createAction(INSERT, text = > ({
+export const insert = createAction(INSERT, text => ({
     id: id++,
     text,
     done:false,
@@ -20,6 +20,41 @@ export const insert = createAction(INSERT, text = > ({
 export const toggle = createAction(TOGGLE, id => id);
 
 export const remove = createAction(REMOVE, id=> id);
+
+// 초기 상태 및 리듀서 함수 만들기
+const initialState = {
+    input: '',
+    todos : [
+        {
+            id:1,
+            text: '리덕스 기초 배우기',
+            done: true
+        },
+        {
+            id: 2,
+            text : '리액트와 리덕스 사용하기',
+            done : false
+        }
+    ]
+};
+
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, {payload : input})=> ({...state, input}),
+        [INSERT] : (state, {payload : todo}) => ({ ... state, todos : state.todos.concat(todo)}),
+        [TOGGLE] : (state, {payload : id}) => ({
+            ...state,
+            todos : state.todos.map(todo => 
+                todo.id === id ? {...todo, done : !todo.done} : todo,
+            )
+        }),
+        [REMOVE] : (state, {payload : id}) => ({
+            ...state,
+            todos : state.todos.filter(todo => todo.id !== id),
+        }),
+    },
+    initialState,
+);
 /*
 // 액션 생성함수 만들기
 export const changeInput = input => ({
